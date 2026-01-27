bbdfbf1550016d10b03a118cff104ac7ad6a639f
JamesChampionGitHub <james@jameschampion.xyz>
2026-01-27 13:58:10 +0000

    string to describe toggle updated

--- ttysh.sh
+++ ttysh.sh
@@ -2441,7 +2441,7 @@ while [ 1 ]; do
 #read -e -p "Enter your selection: " intro
 # fzf use
 
-intro="$(printf "\n%s\n%s\n%s\n%s\n%s\n%s\n%s" "(p)lanner" "(s)elect program" "(f)ind program" "(h)elp" "(t)oggle options currently under development" "(config) wizard" "(q)uit" | sed '/^[[:space:]]*$/d;/.*[A-Z]/d;/[A-Z].*/d' | fzf --prompt "TTYSH " --layout=reverse --margin 20% | sed 's/.*(//g;s/).*//g')"
+intro="$(printf "\n%s\n%s\n%s\n%s\n%s\n%s\n%s" "(p)lanner" "(s)elect program" "(f)ind program" "(h)elp" "(t)oggle options" "(config) wizard" "(q)uit" | sed '/^[[:space:]]*$/d;/.*[A-Z]/d;/[A-Z].*/d' | fzf --prompt "TTYSH " --layout=reverse --margin 20% | sed 's/.*(//g;s/).*//g')"
 		
 
 	case "$intro" in
