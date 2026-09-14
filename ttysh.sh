4a4f7c4ef93b8102f38c4071cdeac4b495cb6fcb
JamesChampionGitHub <james@jameschampion.xyz>
2026-09-14 19:29:00 +0100

    maccel test fix

--- ttysh.sh
+++ ttysh.sh
@@ -1674,13 +1674,15 @@ printf "\n%s" ""
 		;;
 		"mouse settings"|mouse)
 		if [[ ! -d /home/"$USER"/maccel ]]; then
-			cd /home/"$USER"
-			git clone https://github.com/Gnarus-G/maccel
-			cd maccel
-			makepkg -si
+			#cd /home/"$USER"
+			#git clone https://github.com/Gnarus-G/maccel
+			#cd maccel
+			#makepkg -si
+			echo "maccel package does NOT exist"
 		else
-			sudo modprobe maccel
+			#sudo modprobe maccel
 			sudo maccel tui
+			#echo "maccel packages DOES exist"
 		fi
 		;;
 		"calender"|cal)
